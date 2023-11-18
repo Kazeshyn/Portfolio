1c56c45f4e04aa53bd7304ff7b3602e4efe26399
Kazeshyn <72757669+Kazeshyn@users.noreply.github.com>
2023-11-18 22:22:27 +0100

    Début du front en mobile first
    Pour dimensions 320 * 480 px (petit smartphones) portrait et paysages.

---
+++ index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Liens et Plugins -->
+    <link rel="stylesheet" href="style.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <!-- Fonts -->
+    <link href="https://fonts.googleapis.com/css2?family=Kanit:wght@500&display=swap" rel="stylesheet">
+    <title>Merouane Idriss</title>
+</head>
+<body>
+    <nav>
+        <a href="./index.php" class="logoheaderlink"><img src="./images/logo-header.png" alt="Logo, retour à l'accueil (page actuelle)." class="logoheader" ></a>
+        <a href="">Portfolio</a>
+        <a href="" class="endflexgap">Contact</a>
+    </nav>
+    <main>
+    <section class="first-section">
+        <img src="./images/photo-accueil-1.jpg" alt="" class="accueilimg">
+        <h1><img src="./images/h1img.png" alt="" class="h1img"></h1>
+    </section>
+    <section class="second-section">
+        <div>
+            <img src="./images/apropos.png" alt="" class="apropos">
+            <img src="./images/imgapropos.jpg" alt="" class="imgapropos">
+        </div>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus, veniam quod repudiandae ipsum quidem illum perferendis magnam in quo ratione. Dolor accusantium corporis ipsa aperiam laboriosam, beatae sint amet laudantium! Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore error perspiciatis voluptas neque molestiae deleniti delectus nihil cum quam eaque, minus dolores, exercitationem at sed praesentium mollitia molestias repellendus tempora!</p>
+    </section>
+    <section class="third-section">
+        
+    </section>
+    </main>
+    <footer>
+
+    </footer>
+</body>
+</html>
