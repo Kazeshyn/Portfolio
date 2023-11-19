71156a432f1cf4c5fdd70b9e714d45fe838f9cf4
Kazeshyn <idriss.merouane@gmail.com>
2023-11-19 19:39:36 +0100

    :art:
    Fin de la page d'accueil

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="fr">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -11,30 +12,64 @@
     <link href="https://fonts.googleapis.com/css2?family=Kanit:wght@500&display=swap" rel="stylesheet">
     <title>Merouane Idriss</title>
 </head>
+
 <body>
     <nav>
-        <a href="./index.php" class="logoheaderlink"><img src="./images/logo-header.png" alt="Logo, retour à l'accueil (page actuelle)." class="logoheader" ></a>
-        <a href="">Portfolio</a>
-        <a href="" class="endflexgap">Contact</a>
+        <a href="./index.php" class="logoheaderlink"><img src="./images/logo-header.png" alt="Logo, retour à l'accueil (page actuelle)." class="logoheader"></a>
+        <ul>
+        <li><a href="">Portfolio</a><a href="" class="endflexgap">Contact</a></li>
+        </ul>
     </nav>
     <main>
-    <section class="first-section">
-        <img src="./images/photo-accueil-1.jpg" alt="" class="accueilimg">
-        <h1><img src="./images/h1img.png" alt="" class="h1img"></h1>
-    </section>
-    <section class="second-section">
-        <div>
-            <img src="./images/apropos.png" alt="" class="apropos">
-            <img src="./images/imgapropos.jpg" alt="" class="imgapropos">
-        </div>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus, veniam quod repudiandae ipsum quidem illum perferendis magnam in quo ratione. Dolor accusantium corporis ipsa aperiam laboriosam, beatae sint amet laudantium! Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore error perspiciatis voluptas neque molestiae deleniti delectus nihil cum quam eaque, minus dolores, exercitationem at sed praesentium mollitia molestias repellendus tempora!</p>
-    </section>
-    <section class="third-section">
-        
-    </section>
+        <section class="first-section">
+            <img src="./images/photo-accueil-1.jpg" alt="" class="accueilimg">
+            <h1><img src="./images/h1img.png" alt="" class="h1img"></h1>
+        </section>
+        <section class="second-section">
+            <div>
+                <img src="./images/apropos.png" alt="" class="apropos">
+                <img src="./images/imgapropos.jpg" alt="" class="imgapropos">
+            </div>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus, veniam quod repudiandae ipsum quidem illum perferendis magnam in quo ratione. Dolor accusantium corporis ipsa aperiam laboriosam, beatae sint amet laudantium! Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore error perspiciatis voluptas neque molestiae deleniti delectus nihil cum quam eaque, minus dolores, exercitationem at sed praesentium mollitia molestias repellendus tempora!</p>
+        </section>
+        <section class="third-section">
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus, veniam quod repudiandae ipsum quidem illum perferendis magnam in quo ratione. Dolor accusantium corporis ipsa aperiam laboriosam, beatae sint amet laudantium!
+            </p>
+            <div>
+                <img src="./images/cvvtext.png" alt="CV Vidéo" class="cvimage">
+                <iframe width="80%" height="40vh" src="https://www.youtube.com/embed/InMcm5gqHUs" title="CV Vidéo - Benjamin Croizet" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+            </div>
+        </section>
+        <section class="fourth-section">
+            <img src="./images/portfoliotext.png" alt="Portfolio" class="imageportfolio">
+            <div class="gridportfolio">
+            <!-- slider -->
+            <div id="slider">
+                <figure>
+                    <img src="./images/sliderfill.png" alt>
+                    <img src="./images/sliderfill.png" alt>
+                    <img src="./images/sliderfill.png" alt>
+                    <img src="./images/sliderfill.png" alt>
+                    <img src="./images/sliderfill.png" alt>
+                </figure>
+                <!-- fin slider -->
+                </div>
+                <div>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Non doloribus impedit cupiditate fuga ut sint animi commodi ducimus quam, nostrum odit ea labore, adipisci dignissimos iste quia dolorem libero perspiciatis!</p>
+                <div class="divbutton">
+                    <a href="" class="textbutton">Portfolio > </a>
+                </div>
+                </div>
+            </div>
+        </section>
     </main>
     <footer>
-
+        <a href="" class="gapfooter">Plan du site</a>
+        <a href="">Mentions légales</a>
+        <a href="">Contact</a>
+        <a href="./index.php" class="logoheaderlink"><img src="./images/logo-header.png" alt="Logo, retour à l'accueil (page actuelle)." class="logofooter"></a>
     </footer>
 </body>
+
 </html>
